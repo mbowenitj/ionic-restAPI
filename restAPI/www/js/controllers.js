6c1660062b21ea6006455c06a950cf9fadfab239
Tshepo Mboweni <tshepom@mannmademedia.com>
2017-02-27 12:34:49 +0200

    working on people list template

---
+++ restAPI/www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+
+})
+
+
+.controller('People-listCtrl', function($scope, $stateParams) {
+})
+
+.controller('SingleViewCtrl', function($scope) {
+
+});
