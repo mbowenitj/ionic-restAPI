68cb5df76cd9dbe4302167652213f57e4fbee648
Tshepo Mboweni <tshepom@mannmademedia.com>
2017-03-02 17:37:51 +0200

    comic store maps implemented

--- restAPI/www/js/controllers.js
+++ restAPI/www/js/controllers.js
@@ -397,41 +397,66 @@ angular.module('starter')
 }])
 
 .controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
-  var options = {timeout: 10000, enableHighAccuracy: true};
-
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-    var mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-  }, function(error){
-    console.log("Could not get location");
-  });
-
-  //Wait until the map is loaded
-  google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-
-    var marker = new google.maps.Marker({
-        map: $scope.map,
-        animation: google.maps.Animation.DROP,
-        position: latLng
-    });
-
-    var infoWindow = new google.maps.InfoWindow({
-        content: "Here I am!"
-    });
-
-    google.maps.event.addListener(marker, 'click', function () {
-        infoWindow.open($scope.map, marker);
-    });
-
-  });
+  // var options = {timeout: 10000, enableHighAccuracy: true};
+  //
+  // $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+  //
+  //   var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  //
+  //   var mapOptions = {
+  //     center: latLng,
+  //     zoom: 15,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
+  //   };
+  //
+  //   $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  //
+  // }, function(error){
+  //   console.log("Could not get location");
+  // });
+  //
+  // //Wait until the map is loaded
+  // google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+  //
+  //   var marker = new google.maps.Marker({
+  //       map: $scope.map,
+  //       animation: google.maps.Animation.DROP,
+  //       position: latLng
+  //   });
+  //
+  //   var infoWindow = new google.maps.InfoWindow({
+  //       content: "Here I am!"
+  //   });
+  //
+  //   google.maps.event.addListener(marker, 'click', function () {
+  //       infoWindow.open($scope.map, marker);
+  //   });
+  //
+  // });
+  // $scope.positions = [{
+  //   lat: 43.07493,
+  //   lng: -89.381388
+  // }];
+  //
+  // $scope.$on('mapInitialized', function(event, map) {
+  //   $scope.map = map;
+  // });
+  //
+  // $scope.centerOnMe= function(){
+  //
+  //   $ionicLoading.show({
+  //     template: 'Loading...'
+  //   });
+  //
+  //
+  //   navigator.geolocation.getCurrentPosition(function(position) {
+  //     var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  //     $scope.positions.push({lat: pos.k,lng: pos.B});
+  //     console.log(pos);
+  //     $scope.map.setCenter(pos);
+  //     $ionicLoading.hide();
+  //   });
+  //
+  // };
 
 });
